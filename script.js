603053b40829b2558d720cc33838c80bbdc438e0
jonas-45 <jonaswullah@gmail.com>
2023-01-03 08:22:10 +0000

    Add navigation cancel button

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ var navIcon = document.querySelector('.nav-icon');
 var mobileNav = document.querySelector('.nav-wrapper');
 var closeBtn = document.createElement('a');
 var mobileNavBg = document.createElement('div');
+
+closeBtn.classList='x-btn'; 
+
+
+//Hiding Nav Icon
+navIcon.addEventListener('click', vanish);
+navIcon.addEventListener('click', showMenu);
+closeBtn.addEventListener('click', hideMenu);
+
+function vanish(e){
+    navIcon.classList.toggle('mobile-nav'); 
+    
+    //unhide all mbile nav items
+    mobileNav.classList.toggle('unhide-nav', 'nav-wrapper');
+    
+}
