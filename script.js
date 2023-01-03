6d0ec21f457cf6a2e525fec445824b543fc3e7c5
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-01-03 09:16:25 +0100

    Declared variables in JS file.

---
+++ script.js
@@ -0,0 +1,5 @@
+var container = document.body;
+var navIcon = document.querySelector('.nav-icon');
+var mobileNav = document.querySelector('.nav-wrapper');
+var closeBtn = document.createElement('a');
+var mobileNavBg = document.createElement('div');
