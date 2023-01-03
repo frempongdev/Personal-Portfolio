6d0e1f27b5105bce4d8a559c695d46a0045f633a
Frempong <samankwaa.frempong@gmail.com>
2023-01-03 10:37:39 +0000

    Update js file and npx to clear linter errors

--- script.js
+++ script.js
@@ -3,56 +3,46 @@ const navIcon = document.querySelector('.nav-icon');
 const mobileNav = document.querySelector('.nav-wrapper');
 const closeBtn = document.createElement('a');
 const mobileNavBg = document.createElement('div');
-const menuItem = document.querySelectorAll('.nav-wrapper li a')
+const menuItem = document.querySelectorAll('.nav-wrapper li a');
 
-closeBtn.classList='x-btn'; 
+closeBtn.classList = 'x-btn';
 
-//Hiding Nav Icon
+function vanish() {
+  navIcon.classList.toggle('mobile-nav');
+
+  // unhide all mbile nav items
+  mobileNav.classList.toggle('unhide-nav', 'nav-wrapper');
+}
+
+function showMenu() {
+  const header = document.querySelector('.head-top');
+  container.insertBefore(mobileNavBg, header);
+
+  // styling the backgroung
+  mobileNavBg.classList.add('bg-style');
+  mobileNavBg.classList.toggle('mobile-nav');
+
+  // creating close button
+  closeBtn.innerHTML = 'x';
+  navIcon.parentElement.insertBefore(closeBtn, navIcon);
+}
+
+function hideMenu() {
+  container.removeChild(mobileNavBg);
+  navIcon.classList.toggle('mobile-nav');
+  navIcon.parentElement.removeChild(closeBtn);
+  mobileNav.classList.remove('unhide-nav');
+}
+
+for (let i = 0; i < menuItem.length; i + 1) {
+  menuItem[i].addEventListener('click', () => {
+    hideMenu();
+  });
+}
+
+// calling all listeners
+
+// Hiding Nav Icon
 navIcon.addEventListener('click', vanish);
 navIcon.addEventListener('click', showMenu);
 closeBtn.addEventListener('click', hideMenu);
-
-function vanish(){
-    navIcon.classList.toggle('mobile-nav'); 
-    
-    //unhide all mbile nav items
-    mobileNav.classList.toggle('unhide-nav', 'nav-wrapper');
-}
-
-function showMenu(){
-    const header = document.querySelector('.head-top');
-    container.insertBefore(mobileNavBg, header);
-   
-    // styling the backgroung
-    mobileNavBg.classList.add('bg-style');
-    mobileNavBg.classList.toggle('mobile-nav');
-
-    // creating close button
-    closeBtn.innerHTML='x';
-    navIcon.parentElement.insertBefore(closeBtn, navIcon);
-}
-
-function hideMenu(){
-    container.removeChild(mobileNavBg)
-    navIcon.classList.toggle('mobile-nav'); 
-    navIcon.parentElement.removeChild(closeBtn);
-    mobileNav.classList.remove('unhide-nav');
-}
-
-for(let i=0; i<menuItem.length; i++){
-    menuItem[i].addEventListener('click', function(){
-        hideMenu();
-    });
-    
-
-}
-
-
-
-
-
-
-
-
-// function menuListener(e){
-// }
