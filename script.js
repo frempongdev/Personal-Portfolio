082550c01d8489c1ec451a7826c30581af9cded5
Frempong <samankwaa.frempong@gmail.com>
2023-01-03 08:27:33 +0000

    Update JS file with final events

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ var mobileNavBg = document.createElement('div');
 
 closeBtn.classList='x-btn'; 
 
-
 //Hiding Nav Icon
 navIcon.addEventListener('click', vanish);
 navIcon.addEventListener('click', showMenu);
@@ -17,5 +16,24 @@ function vanish(e){
     
     //unhide all mbile nav items
     mobileNav.classList.toggle('unhide-nav', 'nav-wrapper');
-    
+}
+
+function showMenu(e){
+    var header = document.querySelector('.head-top');
+    container.insertBefore(mobileNavBg, header);
+   
+    // styling the backgroung
+    mobileNavBg.classList.add('bg-style');
+    mobileNavBg.classList.toggle('mobile-nav');
+
+    // creating close button
+    closeBtn.innerHTML='x';
+    navIcon.parentElement.insertBefore(closeBtn, navIcon);
+}
+
+function hideMenu(e){
+    container.removeChild(mobileNavBg)
+    navIcon.classList.toggle('mobile-nav'); 
+    navIcon.parentElement.removeChild(closeBtn);
+    mobileNav.classList.remove('unhide-nav');
 }
